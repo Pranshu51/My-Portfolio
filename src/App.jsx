936303a4eb48aebe0e63c06bf1ab075060ae67a0
Pranshu51 <pranshutiwari560@gmail.com>
2026-01-16 00:19:52 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div >
-     <h1 className='text-amber-200'>Welcome to MacFolio</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
